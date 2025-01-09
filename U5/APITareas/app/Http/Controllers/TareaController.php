b2fe50c8d49965fc4b6355dbe2f9319d4f025a7d
fouad2411 <fallaya01@educarex.es>
2025-01-09 10:26:04 +0100

    Terminamos servicios

--- U5/APITareas/app/Http/Controllers/TareaController.php
+++ U5/APITareas/app/Http/Controllers/TareaController.php
@@ -29,7 +29,35 @@ class TareaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //Crear tarea
+        //1º validar datos
+        $request->validate(
+            [
+                'prioridad'=>'in:Alta,Media,Baja',
+                'fecha'=>'required',
+                'hora'=>'required',
+                'descripcion'=>'required',
+            ]
+        );
+
+        try {
+            //Crear objeto tarea
+            $t = new Tarea();
+            if(isset($request->prioridad)){
+
+                $t->prioridad=$request->prioridad;
+            }
+            $t->fecha = $request->fecha;
+            $t->hora = $request->hora;
+            $t->descripcion=$request->descripcion;
+            if($t->save()){
+                return response()->json(['mensaje'=>'Tarea Creada','tarea'=>$t],201);
+            }else{
+                return response()->json('Error: No se ha creado la tarea',500);
+            }    
+        } catch (\Throwable $th) {
+            return response()->json('Error'.$th->getMessage(),500);
+        }
     }
 
     /**
@@ -37,7 +65,14 @@ class TareaController extends Controller
      */
     public function show(Tarea $tarea)
     {
-        //
+        //Mostrar una tarea
+
+        try {
+            //esto daría error 500 qeu es error interno: $a =0/0;
+            return $tarea;
+        } catch (\Throwable $th) {
+            return response()->json('Error'.$th->getMessage(),500);
+        }
     }
 
     /**
@@ -45,7 +80,42 @@ class TareaController extends Controller
      */
     public function update(Request $request, Tarea $tarea)
     {
-        //
+        //Modificar Tarea
+        $request->validate(
+            [
+                'prioridad'=>'in:Alta,Media,Baja'
+            ]
+        );
+
+        try {
+            if(isset($request->fecha) and $tarea->fecha != $request->fecha){
+                $tarea->fecha = $request->fecha;
+            }
+            if(isset($request->hora) and $tarea->hora != $request->hora){
+                $tarea->hora = $request->hora;
+            }
+            if(isset($request->descripcion) and $tarea->descripcion != $request->descripcion){
+                $tarea->descripcion = $request->descripcion;
+            }
+            if(isset($request->prioridad) and $tarea->prioridad != $request->prioridad){
+                $tarea->prioridad = $request->prioridad;
+            }
+            if(isset($request->finalizada) and $tarea->finalizada != $request->finalizada){
+                $tarea->finalizada=$request->finalizada;
+
+            //Guardamos cambios
+
+            }
+            
+            if($tarea->save()){
+                return response()->json(['mensaje'=>'Tarea Creada','tarea'=>$tarea],200);
+            }else{
+                return response()->json('Error: No se ha creado la tarea',500);
+            }    
+
+        } catch (\Throwable $th) {
+            return response()->json('Error'.$th->getMessage(),500);
+        }
     }
 
     /**
@@ -53,6 +123,13 @@ class TareaController extends Controller
      */
     public function destroy(Tarea $tarea)
     {
-        //
+        //Borrar Tarea
+        try {
+            if($tarea->delete()){
+                return response()->json('Tarea Borrada',204);
+            }
+        } catch (\Throwable $th) {
+            return response()->json('Error'.$th->getMessage(),500);
+        }
     }
 }
