aae9a7c17fd5a03663c1de5806bd3e3833020398
fouad2411 <fallaya01@educarex.es>
2025-01-14 10:20:42 +0100

    Nueva tienda

---
+++ U5/APITienda/routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PedidoController;
+use App\Http\Controllers\ProductoController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+//Rutas sin autentificación
+Route::post('login',[LoginController::class,'login']);
+Route::post('registro',[LoginController::class,'registro']);
+
+//Rutas con autentificación
+Route::post('logout',[LoginController::class,'logout'])->middleware('auth:sanctum');
+Route::get('pedidos',[PedidoController::class,'index'])->middleware('auth:sanctum');
+Route::get('productos',[ProductoController::class,'index'])->middleware('auth:sanctum');
+Route::post('pedidos',[PedidoController::class,'store'])->middleware('auth:sanctum');
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
