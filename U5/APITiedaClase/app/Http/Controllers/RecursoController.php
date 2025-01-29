a09c3cfe59318e5d87ef3cc0955931607df0d20b
fouad2411 <fallaya01@educarex.es>
2025-01-29 12:38:35 +0100

    INicio unidad 5

---
+++ U5/APITiedaClase/app/Http/Controllers/RecursoController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Recurso;
+use Illuminate\Http\Request;
+
+class RecursoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Recurso $recurso)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Recurso $recurso)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Recurso $recurso)
+    {
+        //
+    }
+}
---
+++ U5/APITiedaClase/app/Http/Controllers/ReservaController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Reserva;
+use Illuminate\Http\Request;
+
+class ReservaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Reserva $reserva)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Reserva $reserva)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Reserva $reserva)
+    {
+        //
+    }
+}
